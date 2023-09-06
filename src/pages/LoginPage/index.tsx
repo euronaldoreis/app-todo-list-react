1c800e7b890926fd08ba5e5ac034e013f75996b0
Ronaldo Reis <ronaldo.junioaraujodosreis@fit-tecnologia.org.br>
2023-09-06 16:44:47 -0400

    create login page

---
+++ src/pages/LoginPage/index.tsx
@@ -0,0 +1,49 @@
+import React, { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom'
+import { Container, Box, Typography, Button } from '@mui/material';
+import { Google } from '@mui/icons-material';
+import { UserAuth } from '../../context/AuthContext';
+
+const LoginPage = () => {
+
+  const { googleSignIn, user }: any = UserAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const GoogleSignIn = async () => {
+    try {
+      await googleSignIn();
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    console.log(location);
+    if (user !== null) {
+      navigate('/app-todo-list-react/list')
+    }
+  }, [user])
+
+  return (
+    <Container maxWidth="md">
+      <Box mt={5}>
+        <Typography variant="h4" component="div">
+          Lista de Tarefas
+        </Typography>
+      </Box>
+      <Box mt={5}>
+        <Typography variant="h6" component="h6">
+          Faça o login com sua conta Google para continuar.
+        </Typography>
+      </Box>
+      <Box mt={2}>
+        <Button variant="contained" startIcon={<Google />} onClick={GoogleSignIn}>
+          Login com Google
+        </Button>
+      </Box>
+    </Container>
+  )
+}
+
+export default LoginPage;
